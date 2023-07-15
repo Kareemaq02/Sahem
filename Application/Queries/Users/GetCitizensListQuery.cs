36511e00f424f8a7c91ff5d80d0e11179e0d03ec
Kareemaq02 <kareemabuqaoud@hotmail.com>
2023-07-15 17:55:57 +0300

    UsersController- Added the option to get all the citizens and and some of their details.

---
+++ Application/Queries/Users/GetCitizensListQuery.cs
@@ -0,0 +1,9 @@
+﻿using Application.Core;
+using Domain.ClientDTOs.User;
+using Domain.DataModels.User;
+using MediatR;
+
+namespace Application.Queries.Users
+{
+    public record GetCitizensListQuery() : IRequest<Result<List<CitizenDTO>>>;
+}
