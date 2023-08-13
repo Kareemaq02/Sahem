e9b584e06b633ff8fce2ad08ab9c2e0963887db4
Ibrahim Almatar <otakuman789@gmail.com>
2023-08-13 20:06:28 +0300

    edit in FormChip that used in the filters

---
+++ client/src/Common/Components/UI/FormFields/FormChipSelect.jsx
@@ -0,0 +1,38 @@
+// FormSelect.js
+import React from "react";
+import { Chip, Divider } from "@mui/material";
+
+const FormChipSelect = ({ value, onChange, items }) => {
+  const handleChipClick = (item) => {
+    // Check if the item is already selected
+    const isSelected = value.includes(item.value);
+
+    // Toggle the selected status
+    const selectedValues = isSelected
+      ? value.filter((val) => val !== item.value)
+      : [...value, item.value];
+
+    // Call the onChange prop with the selected values
+    onChange(selectedValues);
+  };
+
+  return (
+    <>
+      {items.map((item) => (
+        <>
+        <Chip
+          key={item.value}
+          label={item.label}
+          color={item.color}
+          onClick={() => handleChipClick(item)}
+          variant={value.includes(item.value) ? "default" : "outlined"}
+          />
+          <br/>
+          <br/>
+          </>     
+          ))}
+    </>
+  );
+};
+
+export default FormChipSelect;
