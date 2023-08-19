15945744a68d4cbcd509fe88de626861c61375d2
rubarumman <rubaaburumman.1@gmail.com>
2023-08-19 17:01:19 +0300

    design myComaplint Pages

--- Mobile/citizenUi/account/lib/API/view_complaint_request.dart
+++ Mobile/citizenUi/account/lib/API/view_complaint_request.dart
@@ -6,28 +6,29 @@
 import 'dart:convert';
 
 //import 'package:account/API/login_request.dart';
+import 'package:account/API/login_request.dart';
 import 'package:http/http.dart' as http;
 
 
 class ComplaintModel {
-  final int intId;
-  final String strUserName;
-  final String dtmDateCreated;
-  final String dtmDateFinished;
-  final String strComplaintTypeEn;
-  final String strComplaintTypeAr;
-  final String? strComment;
-  final String? strStatus;
-  final  intPrivacyId;
-  final  intVotersCount;
-  final  decLat;
-  final  decLng;
-  final int decPriority;
-  final List<dynamic> lstMedia;
-  final bool blnIsVideo;
+  int intComplaintId;
+  String strUserName;
+  DateTime dtmDateCreated;
+  DateTime dtmDateFinished;
+  String strComplaintTypeEn;
+  String strComplaintTypeAr;
+  String strComment;
+  String strStatus;
+  int intPrivacyId;
+  String strPrivacyAr;
+  String strPrivacyEn;
+  int intVotersCount;
+  int intVoted;
+  LatLng latLng;
+  List<LstMedia> lstMedia;
 
   ComplaintModel({
-    required this.intId,
+    required this.intComplaintId,
     required this.strUserName,
     required this.dtmDateCreated,
     required this.dtmDateFinished,
@@ -36,39 +37,74 @@ class ComplaintModel {
     required this.strComment,
     required this.strStatus,
     required this.intPrivacyId,
+    required this.strPrivacyAr,
+    required this.strPrivacyEn,
     required this.intVotersCount,
-    required this.decLat,
-    required this.decLng,
-    required this.decPriority,
+    required this.intVoted,
+    required this.latLng,
     required this.lstMedia,
-    required this.blnIsVideo,
   });
 
   factory ComplaintModel.fromJson(Map<String, dynamic> json) {
     return ComplaintModel(
-      intId: json['intComplaintId'],
+      intComplaintId: json['intComplaintId'],
       strUserName: json['strUserName'],
-      dtmDateCreated: json['dtmDateCreated'],
-      dtmDateFinished: json['dtmDateFinished'],
+      dtmDateCreated: DateTime.parse(json['dtmDateCreated']),
+      dtmDateFinished: DateTime.parse(json['dtmDateFinished']),
       strComplaintTypeEn: json['strComplaintTypeEn'],
       strComplaintTypeAr: json['strComplaintTypeAr'],
       strComment: json['strComment'],
       strStatus: json['strStatus'],
       intPrivacyId: json['intPrivacyId'],
+      strPrivacyAr: json['strPrivacyAr'],
+      strPrivacyEn: json['strPrivacyEn'],
       intVotersCount: json['intVotersCount'],
-      decLat: json['decLat'],
-      decLng: json['decLng'],
-      decPriority: json['decPriority'],
-      lstMedia: json['lstMedia'],
-      blnIsVideo: json['blnIsVideo'],
+      intVoted: json['intVoted'],
+      latLng: LatLng(
+        decLat: json['latLng']['decLat'],
+        decLng: json['latLng']['decLng'],
+      ),
+      lstMedia: (json['lstMedia'] as List<dynamic>)
+          .map((media) => LstMedia.fromJson(media))
+          .toList(),
     );
   }
 }
 
+class LatLng {
+  double decLat;
+  double decLng;
+
+  LatLng({
+    required this.decLat,
+    required this.decLng,
+  });
+}
+
+class LstMedia {
+  String data;
+  bool isVideo;
+
+  LstMedia({
+    required this.data,
+    required this.isVideo,
+  });
+
+  factory LstMedia.fromJson(Map<String, dynamic> json) {
+    return LstMedia(
+      data: json['data'],
+      isVideo: json['isVideo'],
+    );
+  }
+}
+
+
+
+
 class getUserComplaint {
   
   Future<List<ComplaintModel>> getComplaintById(String complaintId) async {
-     String token2='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXJuYW1lIiwiZmlyc3ROYW1lIjoiZmlyc3QiLCJsYXN0TmFtZSI6Imxhc3QiLCJwaG9uZU51bWJlciI6IjAxMjM0NTY3ODkiLCJ1c2VyVHlwZSI6InVzZXIiLCJuYmYiOjE2ODg2NTEwMDYsImV4cCI6MTY5MTI0MzAwNiwiaWF0IjoxNjg4NjUxMDA2fQ.NJPnHG4WNtnelTqJm7KNGY4Jf6j3j7XZ5zOMHpALDBM';
+    // String token2='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXJuYW1lIiwiZmlyc3ROYW1lIjoiZmlyc3QiLCJsYXN0TmFtZSI6Imxhc3QiLCJwaG9uZU51bWJlciI6IjAxMjM0NTY3ODkiLCJ1c2VyVHlwZSI6InVzZXIiLCJuYmYiOjE2ODg2NTEwMDYsImV4cCI6MTY5MTI0MzAwNiwiaWF0IjoxNjg4NjUxMDA2fQ.NJPnHG4WNtnelTqJm7KNGY4Jf6j3j7XZ5zOMHpALDBM';
     var baseUrl = "https://10.0.2.2:5000/api/complaints/$complaintId";
     print(complaintId);
     http.Response response = await http.get(Uri.parse(baseUrl), headers: {'Authorization': 'Bearer $token2'});
