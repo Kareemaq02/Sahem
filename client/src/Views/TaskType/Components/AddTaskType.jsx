291598a859ad796f91fc0bc1155ac0903ec403f1
Ibrahim Almatar <otakuman789@gmail.com>
2023-08-20 22:07:07 +0300

    new design

--- client/src/Views/TaskType/Components/AddTaskType.jsx
+++ client/src/Views/TaskType/Components/AddTaskType.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, TextField } from "@mui/material";
+import { Box, Button, Paper, TextField, Typography } from "@mui/material";
 import FormTextField from "../../../Common/Components/UI/FormFields/FormTextField";
 import { FormProvider, useForm } from "react-hook-form";
 import InsertTaskType from "../Service/InsertTaskType";
@@ -18,27 +18,30 @@ const AddTaskType = () => {
     }
   };
   return (
-    <FormProvider {...methods}>
-      <form onSubmit={methods.handleSubmit(onSubmit)}>
-        <FormTextField name="intDepartmentId" label="Department ID" />
-        <br />
-        <br />
-        <FormTextField name="strNameAr" label="Arabic Name" />
-        <br />
-        <br />
-        <FormTextField name="strNameEn" label="English Name" />
-        <br />
-        <br />
-        <Button
-          type="submit"
-          variant="contained"
-          color="primary"
-          sx={{ width: "100%" }}
-        >
-          Add
-        </Button>
-      </form>
-    </FormProvider>
+    <Paper sx={{p:2, borderRadius:'25px'}}>
+      <Typography variant="h2" sx={{p:1, textAlign:'center'}}>Insert Task Type</Typography>
+      <FormProvider {...methods}>
+        <form onSubmit={methods.handleSubmit(onSubmit)}>
+          <FormTextField name="intDepartmentId" label="Department ID" />
+          <br />
+          <br />
+          <FormTextField name="strNameAr" label="Arabic Name" />
+          <br />
+          <br />
+          <FormTextField name="strNameEn" label="English Name" />
+          <br />
+          <br />
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            sx={{ width: "100%" }}
+          >
+            Add
+          </Button>
+        </form>
+      </FormProvider>
+    </Paper>
   );
 };
 
--- client/src/Views/TaskType/Views/AdminTaskView.jsx
+++ client/src/Views/TaskType/Views/AdminTaskView.jsx
@@ -1,26 +1,33 @@
-import { Typography, SwipeableDrawer } from "@mui/material";
+import { Typography, SwipeableDrawer, Grid } from "@mui/material";
 import TaskTypeDataGrid from "../Components/TaskTypeDataGrid";
 import GetTaskType from "../Service/GetTaskType"
 import { useEffect, useState } from "react";
-
+import AddTaskType from "../Components/AddTaskType";
 
 
 function AdminTaskType() {
-    const [complaints, setComplaints] = useState([]);
+  const [complaints, setComplaints] = useState([]);
 
-    useEffect(() => {
-        const setTaskType = async () => {
-            const response = await GetTaskType();
-            setComplaints(response.data)
-        };
-        setTaskType();
-    },[])
+  useEffect(() => {
+    const setTaskType = async () => {
+      const response = await GetTaskType();
+      setComplaints(response.data)
+    };
+    setTaskType();
+  }, [])
 
 
   return (
     <div>
       <Typography variant="h1">View Task Type</Typography>
-      <TaskTypeDataGrid  data={complaints}/>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={8}>
+          <TaskTypeDataGrid data={complaints} />
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <AddTaskType />
+        </Grid>
+      </Grid>
     </div>
   );
 }
