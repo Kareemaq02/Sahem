bddbe699eac88e45903f1bbf0091f5464220f45d
Kareemaq02 <kareemabuqaoud@hotmail.com>
2023-08-25 14:06:58 +0300

    Tasks - A handler that gets the complaints related to a task.

---
+++ Application/Queries/Complaints/GetComplaintsByTaskIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using Application.Core;
+using Domain.ClientDTOs.Complaint;
+using MediatR;
+
+namespace Application.Queries.Complaints
+{
+    public record GetComplaintsByTaskIdQuery(int intTaskId)
+        : IRequest<Result<List<ComplaintViewDTO>>>;
+}
