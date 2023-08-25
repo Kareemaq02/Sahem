581fea69cab296bedff0ac8c7f14f73050b101a6
Kareemaq02 <kareemabuqaoud@hotmail.com>
2023-08-25 18:15:44 +0300

    Profession Controller - Added a handler that allows the admin to soft delete a department

---
+++ Application/Commands/RemoveDepartmentByIdCommand.cs
@@ -0,0 +1,8 @@
+﻿using Application.Core;
+using MediatR;
+
+namespace Application.Commands
+{
+    public record RemoveDepartmentByIdCommand(int intDepartmentId, string username)
+        : IRequest<Result<string>>;
+}
