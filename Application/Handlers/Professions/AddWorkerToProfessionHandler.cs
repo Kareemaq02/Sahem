8c739cd05957e83060fd99968950d89e84158635
Kareemaq02 <kareemabuqaoud@hotmail.com>
2023-08-25 18:15:20 +0300

    Profession Controller - Added a handler that allows the admin to add a worker to the selected profession

---
+++ Application/Handlers/Professions/AddWorkerToProfessionHandler.cs
@@ -0,0 +1,61 @@
+﻿using Application.Core;
+using Domain.ClientDTOs.Department;
+using Domain.DataModels.LookUps;
+using Domain.DataModels.User;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Persistence;
+using Microsoft.EntityFrameworkCore;
+using Domain.DataModels.Intersections;
+
+namespace Application.Handlers.LookUps
+{
+    public class AddWorkerToProfessionHandler
+        : IRequestHandler<AddWorkerToProfessionCommand, Result<string>>
+    {
+        private readonly DataContext _context;
+        private readonly IConfiguration _configuration;
+        public readonly UserManager<ApplicationUser> _userManager;
+
+        public AddWorkerToProfessionHandler(
+            DataContext context,
+            IConfiguration configuration,
+            UserManager<ApplicationUser> userManager
+        )
+        {
+            _context = context;
+            _configuration = configuration;
+            _userManager = userManager;
+        }
+
+        public async Task<Result<string>> Handle(
+            AddWorkerToProfessionCommand request,
+            CancellationToken cancellationToken
+        )
+        {
+
+            if (
+                _context.ProfessionUsers.Any(
+                    d =>
+                        d.intUserId == request.intWorkerId
+                        && d.intProfessionId == request.intProfessionId
+                )
+            )
+                return Result<string>.Failure("Worker is already in specified profession");
+
+
+
+            var professionUser = new ProfessionUsers
+            {
+                intUserId = request.intWorkerId,
+                intProfessionId = request.intProfessionId
+            };
+
+            await _context.ProfessionUsers.AddAsync(professionUser, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Result<string>.Success("Worker added successfully");
+        }
+    }
+}
