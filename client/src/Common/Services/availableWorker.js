01ddd71bb3e034036d9654d73de9bf660fb42199
Ibrahim Almatar <otakuman789@gmail.com>
2023-08-27 00:15:27 +0300

    new Api requests

---
+++ client/src/Common/Services/availableWorker.js
@@ -0,0 +1,15 @@
+import axios from "../../Common/Utils/AxiosAgent";
+
+export const availableWorker = async (startDate, dueDate) => {
+  try {
+    const response = await axios.get(
+      `https://localhost:5000/api/users/workers/available?startDate=${startDate}&endDate=${dueDate}`
+    );
+    return response.data.map((employee) => ({
+      intId: employee.intId,
+      strName: employee.strFirstName + " " + employee.strLastName,
+    }));
+  } catch (error) {
+    console.error(error);
+  }
+};
