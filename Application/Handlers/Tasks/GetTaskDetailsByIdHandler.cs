9e3cf1275b0855fee95b5b9c5142ca38b6d2fe0c
Kareemaq02 <kareemabuqaoud@hotmail.com>
2023-09-05 23:34:02 +0300

    TasksController - Changed all handlers so that they match the new teams concept.

--- Application/Handlers/Tasks/GetTaskDetailsByIdHandler.cs
+++ Application/Handlers/Tasks/GetTaskDetailsByIdHandler.cs
@@ -1,10 +1,8 @@
 ﻿using Application.Core;
 using Application.Queries.Complaints;
-using Application.Queries.Tasks;
-using Domain.ClientDTOs.Complaint;
 using Domain.ClientDTOs.Task;
 using Domain.ClientDTOs.User;
-using Domain.DataModels.Tasks;
+using Domain.Helpers;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
@@ -41,60 +39,41 @@ namespace Application.Handlers.Tasks
 
             var query =
                 from t in _context.Tasks
-                join u in _context.Users on t.intAdminId equals u.Id
-                join ui in _context.UserInfos on u.intUserInfoId equals ui.intId
+                join ui in _context.UserInfos on t.intAdminId equals ui.intId
                 join tT in _context.TaskTypes on t.intTypeId equals tT.intId
                 join ts in _context.TaskStatus on t.intStatusId equals ts.intId
-                join tm in _context.TeamMembers on t.intId equals tm.intTeamId
+                join tm in _context.Teams on t.intTeamId equals tm.intId
                 where t.intId == request.Id
-                group tm by new
-                {
-                    TaskID = t.intId,
-                    TaskTypeEn = tT.strNameEn,
-                    TaskTypeAr = tT.strNameAr,
-                    CreationDate = t.dtmDateCreated,
-                    ActivationDate = t.dtmDateActivated,
-                    FinishedDate = t.dtmDateFinished,
-                    ScheduledDate = t.dtmDateScheduled,
-                    LastModifiedDate = t.dtmDateLastModified,
-                    Comment = t.strComment,
-                    DeadlineDate = t.dtmDateDeadline,
-                    TaskStatus = ts.strName,
-                    Cost = t.decCost,
-                    UserRating = t.decUserRating,
-                    AdminFirstName = ui.strFirstName,
-                    AdminLastName = ui.strLastName,
-                } into g
                 select new DetailedTaskDTO
                 {
-                    taskID = g.Key.TaskID,
-                    createdDate = g.Key.CreationDate,
-                    strTypeNameEn = g.Key.TaskTypeEn,
-                    strTypeNameAr = g.Key.TaskTypeAr,
-                    activatedDate = g.Key.ActivationDate,
-                    lastModifiedDate = g.Key.LastModifiedDate,
-                    strComment = g.Key.Comment,
-                    strAdminFirstName = g.Key.AdminFirstName,
-                    strAdminLastName = g.Key.AdminLastName,
-                    decCost = g.Key.Cost,
-                    decUserRating = g.Key.UserRating,
-                    finishedDate = g.Key.FinishedDate,
-                    scheduledDate = g.Key.ScheduledDate,
-                    deadlineDate = g.Key.DeadlineDate,
-                    strTaskStatus = g.Key.TaskStatus,
-                    workersList = g.Select(
-                            x =>
-                                new TaskWorkerDTO
-                                {
-                                    intId = x.Worker.Id,
-                                    //isLeader = x.blnIsLeader,
-                                    strFirstName = x.Worker.UserInfo.strFirstName,
-                                    strLastName = x.Worker.UserInfo.strLastName
-                                }
-                        )
-                        .Distinct()
-                        .ToList(),
-                    lstMedia = lstMedia
+                    taskID = t.intId,
+                    createdDate = t.dtmDateCreated,
+                    strTypeNameEn = tT.strNameEn,
+                    strTypeNameAr = tT.strNameAr,
+                    activatedDate = t.dtmDateActivated,
+                    lastModifiedDate = t.dtmDateLastModified,
+                    strComment = t.strComment,
+                    strAdminFirstName = ui.strFirstName,
+                    strAdminLastName = ui.strLastName,
+                    decCost = t.decCost,
+                    decUserRating = t.decUserRating,
+                    finishedDate = t.dtmDateFinished,
+                    scheduledDate = t.dtmDateScheduled,
+                    deadlineDate = t.dtmDateDeadline,
+                    strTaskStatus = ts.strName,
+                    intTeamId = t.intTeamId,
+                    workersList = t.Team.Workers
+                                .Select(
+                                    ca =>
+                                        new TaskWorkerDTO
+                                        {
+                                            intId = ca.intWorkerId,
+                                            strFirstName = ca.Worker.UserInfo.strFirstName,
+                                            strLastName = ca.Worker.UserInfo.strLastName,
+                                            isLeader = t.Team.intLeaderId == ca.intWorkerId
+                                        }
+                                )
+                                .ToList()
                 };
 
             var result = await query.FirstOrDefaultAsync();
--- Domain/ClientDTOs/Task/TaskListDTO.cs
+++ Domain/ClientDTOs/Task/TaskListDTO.cs
@@ -5,6 +5,9 @@ namespace Domain.ClientDTOs.Task
     public class TaskListDTO
     {
         public int taskID { get; set; }
+        public int intTeamID { get; set; }
+        public int intTaskTypeId { get; set; }
+        public int intTaskStatusId { get; set; }
         public DateTime activatedDate { get; set; }
         public DateTime finishedDate { get; set; }
         public DateTime scheduledDate { get; set; }
@@ -12,10 +15,10 @@ namespace Domain.ClientDTOs.Task
         public string adminUsername { get; set; }
         public string adminName { get; set; }
         public string adminNameAr { get; set; }
-        public int intTaskTypeId { get; set; }
         public string strTypeNameAr { get; set; }
         public string strTypeNameEn { get; set; }
-        public int intTaskStatusId { get; set; }
+        public string strDepartmentAr { get; set; }
+        public string strDepartmentEn { get; set; }
         public string strTaskStatus { get; set; }
         public string strTaskStatusAr { get; set; }
         public List<TaskWorkerDTO> workersList { get; set; }
