662e1ea6cac27c75452ec4bf684ffc57b56c2921
Kareemaq02 <kareemabuqaoud@hotmail.com>
2023-09-05 23:11:23 +0300

    TeamsController - Added a handler that allows the admin to create a team

---
+++ Application/Commands/Teams/CreateTeamCommand.cs
@@ -0,0 +1,10 @@
+﻿using Application.Core;
+using Domain.ClientDTOs.Profession;
+using Domain.ClientDTOs.Team;
+using MediatR;
+
+namespace Application
+{
+    public record CreateTeamCommand(CreateTeamDTO createTeamDTO)
+        : IRequest<Result<CreateTeamDTO>>;
+}
---
+++ Domain/ClientDTOs/Team/CreateTeamDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Domain.ClientDTOs.Team
+{
+    public class CreateTeamDTO
+    {
+        [JsonIgnore]
+        public string strUsername { get; set; }
+        [JsonIgnore]
+        public int intTeamId { get; set; } = 0;
+        public int intDepartmentId { get; set; }
+        public List<TeamMemberDTO> lstTeamMembers { get; set; } = new List<TeamMemberDTO>();
+
+    }
+}
