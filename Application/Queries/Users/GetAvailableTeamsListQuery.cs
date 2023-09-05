e343c8303a074f15f9712ead11d2ed436bb12c7f
Kareemaq02 <kareemabuqaoud@hotmail.com>
2023-09-05 23:03:52 +0300

    TeamsController - Added a handler that gets the available teams

---
+++ Application/Queries/Users/GetAvailableTeamsListQuery.cs
@@ -0,0 +1,11 @@
+﻿using Application.Core;
+using Domain.ClientDTOs.Task;
+using Domain.ClientDTOs.User;
+using Domain.DataModels.User;
+using MediatR;
+
+namespace Application.Queries.Users
+{
+    public record GetAvailableTeamsListQuery(DateTime startDate, DateTime endDate) :
+        IRequest<Result<List<TeamDTO>>>;
+}
