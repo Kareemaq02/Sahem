34078aae2beb1dec502b5cd183f13d4f93d23f53
Kareemaq02 <kareemabuqaoud@hotmail.com>
2023-09-05 23:19:00 +0300

    TaskController - Missing queries added.

---
+++ Application/Queries/Teams/GetWorkersWithNoTeamListQuery.cs
@@ -0,0 +1,10 @@
+﻿using Application.Core;
+using Domain.ClientDTOs.User;
+using Domain.DataModels.User;
+using MediatR;
+
+namespace Application.Queries.Teams
+{
+    public record GetWorkersWithNoTeamListQuery(PagingParams PagingParams)
+        : IRequest<Result<PagedList<WorkerDTO>>>;
+}
