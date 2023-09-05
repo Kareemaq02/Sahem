d2ca9c22de5eef8db3a652842aa7383afbd1ac86
Kareemaq02 <kareemabuqaoud@hotmail.com>
2023-09-05 23:16:35 +0300

    TasksController - Added a handler to get the teams list

---
+++ Application/Handlers/Teams/GetTeamByIdHandler.cs
@@ -0,0 +1,47 @@
+﻿using Application.Core;
+using Application.Queries.Teams;
+using Domain.ClientDTOs.Team;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Handlers.Teams
+{
+    public class GetTeamByIdHandler
+        : IRequestHandler<GetTeamByIdQuery, Result<TeamDTO>>
+    {
+        private readonly DataContext _context;
+
+        public GetTeamByIdHandler(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Result<TeamDTO>> Handle(
+            GetTeamByIdQuery request,
+            CancellationToken cancellationToken
+        )
+        {
+            var query = from team in _context.Teams
+                        join d in _context.Departments on team.intDepartmentId equals d.intId
+                        where team.intId == request.intTeamId
+                        select
+                               new TeamDTO
+                               {
+                                   intTeamId = team.intId,
+                                   intDepartmentId = d.intId,
+                                   intTeamLeaderId = team.intLeaderId,
+                                   strDepartmentNameAr = d.strNameAr,
+                                   strDepartmentNameEn = d.strNameEn,
+                                   strTeamLeaderFirstNameAr = team.Leader.UserInfo.strFirstNameAr,
+                                   strTeamLeaderFirstNameEn = team.Leader.UserInfo.strFirstName,
+                                   strTeamLeaderLastNameAr = team.Leader.UserInfo.strLastNameAr,
+                                   strTeamLeaderLastNameEn = team.Leader.UserInfo.strLastName,
+
+                               };
+            var result = await query.FirstOrDefaultAsync();
+
+            return Result<TeamDTO>.Success(result);
+        }
+    }
+}
