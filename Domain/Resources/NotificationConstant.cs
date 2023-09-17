55779ff2c0d9f0db6e93b7e5f3583c7ee855bb32
Kareemaq02 <kareemabuqaoud@hotmail.com>
2023-09-17 16:46:05 +0300

    Database - Added a column to save notification history, added a User Task Rating table, and also added a Task Member Rating table

---
+++ Domain/Resources/NotificationConstant.cs
@@ -0,0 +1,18 @@
+﻿namespace Domain.Resources
+{
+    public static class NotificationConstant
+    {
+        public enum NotificationType
+        {
+            rejectedComplaintNotification = 1,
+            workerAddedToDepartmentNotification = 2,
+            completedComplaintNotification = 3,
+            complaintStatusChangeNotification = 4,
+            taskStatusChangeNotification = 5,
+            taskDeletionNotification = 6,
+            taskCreationNotification = 7,
+            workerAddedToTeamNotification = 9
+        }
+
+    }
+}
