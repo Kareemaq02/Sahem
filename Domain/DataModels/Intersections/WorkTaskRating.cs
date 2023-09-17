becd6b8b072c9b49f35fb1b73d59d1a0fcb18acf
Kareemaq02 <kareemabuqaoud@hotmail.com>
2023-09-17 17:12:16 +0300

    Database - added intersection tables

---
+++ Domain/DataModels/Intersections/WorkTaskRating.cs
@@ -0,0 +1,25 @@
+﻿using Domain.DataModels.Tasks;
+using Domain.DataModels.User;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.DataModels.Intersections
+{
+    [Table("tasks_ratings")]
+    public class WorkTaskRating
+    {
+        [Column("TASK_ID")]
+        [ForeignKey("Task")]
+        public int intTaskId { get; set; }
+        public WorkTask Task { get; set; }
+
+        [Column("USER_ID")]
+        [ForeignKey("User")]
+        public int intUserId { get; set; }
+        public ApplicationUser User { get; set; }
+
+        [Column("RATING")]
+        [Required]
+        public decimal decRating { get; set; }
+    }
+}
